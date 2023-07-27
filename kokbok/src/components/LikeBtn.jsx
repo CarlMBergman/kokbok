c52cf6ef85ea91536e3e2d159af4e24ff95abddc
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2023-07-27 20:12:30 +0200

    likedimg

--- kokbok/src/components/LikeBtn.jsx
+++ kokbok/src/components/LikeBtn.jsx
@@ -1,33 +1,35 @@
 import { useState,useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux";
 import { addRecipe, removeRecipe, localStorageUpdate } from "./action";
+import Liked from '../components/img/liked.svg'
+import notLiked from '../components/img/not-liked.svg'
 
 
 function LikeBtn(props) {
     const dispatch = useDispatch()
     const userRecipes = useSelector((state) => { return state.userRecipes })
-    const [like, setLike] = useState('/src/components/img/not-liked.svg');
+    const [like, setLike] = useState(notLiked);
     const index = userRecipes.findIndex(userRecipes => userRecipes.name === props.currentRecipe.name)
 
     useEffect(() => {
         function checkLiked() {
             if (index == -1) {
-                setLike('/src/components/img/not-liked.svg')
+                setLike(notLiked)
             }
             else {
-                setLike('/src/components/img/liked.svg')
+                setLike(Liked)
             }
         }
         checkLiked()
     })
 
     function onClick() {
-        if (like === '/src/components/img/not-liked.svg') {
-            setLike('/src/components/img/liked.svg')
+        if (like === notLiked) {
+            setLike(Liked)
             dispatch(addRecipe(props.currentRecipe))
         } 
         else {
-            setLike('/src/components/img/not-liked.svg')
+            setLike(notLiked)
             dispatch(removeRecipe(props.currentRecipe))
         }
         dispatch(localStorageUpdate())
